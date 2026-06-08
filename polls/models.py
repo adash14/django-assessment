bd4df57bc3fb9a5683bbfae64a2bdefae3f4415a
adash14 <dash14@purdue.edu>
2026-06-08 13:18:44 -0700

    Add README documentation

--- polls/models.py
+++ polls/models.py
@@ -10,9 +10,9 @@ class Question(models.Model):
     pub_date = models.DateTimeField("date published")
     def __str__(self):
         return self.question_text
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    # def was_published_recently(self):
+    #     now = timezone.now()
+    #     return now - datetime.timedelta(days=1) <= self.pub_date <= now
     @admin.display(
         boolean=True,
         ordering="pub_date",
